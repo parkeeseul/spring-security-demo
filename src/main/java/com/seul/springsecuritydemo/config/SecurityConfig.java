41f6c3147bf1d61ee90f7ed1cdb714ec3ee66333
parkeeseul <eeseul.p@gmail.com>
2021-05-21 14:08:04 +0900

    feat: Spring Security ignoring()

--- src/main/java/com/seul/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/seul/springsecuritydemo/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.seul.springsecuritydemo.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -15,6 +17,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Override
